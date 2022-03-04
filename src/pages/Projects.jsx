6ea41d3af747c46d9b11e6ca56459886d883272c
Jose Acosta <jdap.dmc@gmail.com>
2022-03-04 17:37:21 -0400

    feat: projects page

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,7 +1,34 @@
 import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation } from 'swiper';
+import proyectos from '../types/proyectos';
+import 'swiper/css';
+import 'swiper/css/navigation';
+
+const swiper = {
+	slidesPerView: 1,
+	spaceBetween: 30,
+	loop: true,
+	navigation: true,
+	modules: [Navigation],
+	className: 'mySwiper'
+};
 
 const Projects = () => {
-	return <div>Projects</div>;
+	return (
+		<section className="swiper-container">
+			<h2 className="title">My Projects</h2>
+			<Swiper {...swiper}>
+				{proyectos.map(({ img, key }) => (
+					<SwiperSlide
+						key={key}
+						className="container-swiper-img"
+						style={{ backgroundImage: `url(${img})` }}
+					></SwiperSlide>
+				))}
+			</Swiper>
+		</section>
+	);
 };
 
 export default Projects;
---
+++ src/types/proyectos.js
@@ -0,0 +1,20 @@
+const projects = [
+	{
+		img: '/assets/logo192.png',
+		key: 'react'
+	},
+	{
+		img: '/assets/mypicture.jpg',
+		key: 'yop'
+	},
+	{
+		img: '/assets/logo192.png',
+		key: ':3'
+	},
+	{
+		img: '/assets/mypicture.jpg',
+		key: 'yo'
+	}
+];
+
+export default projects;
